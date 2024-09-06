ae6912b26bbb8d2a2be22f604fd92431afdc0745
Shweta-A96 <reddyshweta96@gmail.com>
2024-09-06 17:21:07 +0530

    Hello123

--- src/sept/ex_04092024/Lab002.java
+++ src/sept/ex_04092024/Lab002.java
@@ -1,4 +1,13 @@
 package sept.ex_04092024;
 
-public class Lab002 {
+public class Lab002
+{
+public static void main(String[] args)
+{
+    System.out.println("mathematics");
+    System.out.println(23 * 26);
+    System.out.println(2+3);
+    System.out.println(50-25);
+    System.out.println(45/2);
+}
 }
