406aa826b2bbe346252657a55710bb66daaac815
Shweta-A96 <reddyshweta96@gmail.com>
2024-09-06 16:52:34 +0530

    Hello123

--- src/sept/ex_04092024/Lab001.java
+++ src/sept/ex_04092024/Lab001.java
@@ -1,4 +1,23 @@
 package sept.ex_04092024;
 
-public class Lab001 {
-}
+public class   Lab001 {
+    public static void main(String[] args) {
+        System.out.println("Hi,Hello");
+        System.out.println("I am shweta");
+        main("shweta");
+    }
+
+    public static void main(String args) {
+        System.out.println("how are you");
+        System.out.println("i am fine");
+
+    }
+        public void main ( int args)
+        {
+            System.out.println("see you soon");
+        }
+    }
+
+
+
+
