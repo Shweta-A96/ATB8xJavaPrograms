c4eb2bdf5003057e2baced55d11a5d1b700b0480
Shweta-A96 <reddyshweta96@gmail.com>
2024-09-17 23:56:23 +0530

    All files of sep 13

---
+++ src/sept/ex_13092024/Calculator.java
@@ -0,0 +1,20 @@
+package sept.ex_13092024;
+
+public class Calculator {
+    public static void main(String[] args)
+    {
+        double a = 20;
+        double b = 40;
+        System.out.printf("Addition of %f and %f is %f", a,b,a+b);
+        System.out.println();
+        System.out.printf("multiplication of %f and %f is %f", a,b,a*b);
+        System.out.println();
+        System.out.printf("substraction of %f and %f is %f", a,b,(a-b));
+        System.out.println();
+        System.out.printf("division of %f and %f is %f", a,b,(a/b));
+        System.out.println();
+        System.out.printf("modulus of %f and %f is %f", a,b,a%b);
+
+
+    }
+}
